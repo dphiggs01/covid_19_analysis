e7a9b9dc04194d821abe9b6bcc7bc33a873210cb
Dan Higgins <daniel.higgins@yahoo.com>
2020-04-01 08:12:25 -0400

    update control script

--- data_prep/covid_19_ny_times_data_prep/control.sh
+++ data_prep/covid_19_ny_times_data_prep/control.sh
@@ -1,2 +1,10 @@
+if [ -d "/home/ubuntu/Applications/python_envs" ]; then
+    source /home/ubuntu/Applications/python_envs/bin/activate
+    PORT=80
+fi
+cd /home/ubuntu/Applications/covid_19_analysis
+git pull
+cd -
+
 python covid_19_cases_by_county.py
 python covid_19_cases_by_state.py
